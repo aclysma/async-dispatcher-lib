bc75199b7b32f72db82ade1331fd6a8e94add217
Philip Degarmo <aclysma@gmail.com>
2019-07-06 22:00:05 -0700

    Cleaned up a bit

--- src/minimum_example.rs
+++ src/minimum_example.rs
@@ -41,93 +41,36 @@ fn acquire_resources_and_use<RequirementT, F>(
 
     Box::new(
         acquire_resources::<RequirementT>(dispatcher.clone(), world.clone())
-
-            //TODO: Both and_then and map work, not sure why or if one is better than the other
-
-//        .and_then(move |acquired_resources| {
-//            (f)(acquired_resources);
-//            Ok(())
-//        })
-
         .map(move |acquired_resources| {
             (f)(acquired_resources);
         })
     )
 }
 
-
-fn acquire_resources_and_visit<'a, RequirementT, F>(
-    dispatcher: Arc<Dispatcher>,
-    world: Arc<World>,
-    f: F
-) -> Box<impl futures::future::Future<Item=(), Error=()>>
-    where
-        RequirementT: RequiresResources + 'static + Send,
-        RequirementT: DataRequirement<'a>,
-        //F : Fn(AcquiredResources<RequirementT>) + 'static,
-        F : FnOnce(<RequirementT as DataRequirement<'a>>::Borrow)
-{
-    use futures::future::Future;
-
-    Box::new(
-        acquire_resources::<RequirementT>(dispatcher.clone(), world.clone())
-
-            //TODO: Both and_then and map work, not sure why or if one is better than the other
-
-//        .and_then(move |acquired_resources| {
-//            (f)(acquired_resources);
-//            Ok(())
-//        })
-
-            .map(move |acquired_resources| {
-                //(f)(acquired_resources);
-                acquired_resources.visit(f);
-            })
-    )
-}
-
-
-fn example_fn2(resources: AcquiredResources<(Read<HelloWorldResourceA>, Write<HelloWorldResourceB>)>) {
+fn example_inline(resources: AcquiredResources<(
+    Read<HelloWorldResourceA>,
+    Write<HelloWorldResourceB>
+)>) {
     resources.visit(|r| {
         let (a, mut b) = r;
         b.value += 1;
     })
 }
-
-
 
 type ExampleFn3Args = (Read<HelloWorldResourceA>, Write<HelloWorldResourceB>);
-fn example_fn3(resources: AcquiredResources<ExampleFn3Args>) {
+fn example_typedef(resources: AcquiredResources<ExampleFn3Args>) {
     resources.visit(|r| {
         let (a, mut b) = r;
         b.value += 1;
     })
 }
 
-
-fn example_fn(resources: AcquiredResources<Write<HelloWorldResourceB>>) {
+fn example_single_resource(resources: AcquiredResources<Write<HelloWorldResourceB>>) {
     resources.visit(|mut b| {
         println!("hi");
         b.value += 1;
     })
 }
-//
-//
-//type ExampleFn3ArgsBorrow = ExampleFn3Args::Borrow;
-//fn example_fn_2(borrow: ExampleFn3ArgsBorrow) {
-//
-//}
-
-fn example_fn_borrow(resources: (ReadBorrow<HelloWorldResourceA>, WriteBorrow<HelloWorldResourceB>)) {
-    let (a, mut b) = resources;
-    b.value += 1;
-}
-
-//fn example_fn_borrow2(resources: <(ReadBorrow<HelloWorldResourceA>, WriteBorrow<HelloWorldResourceB>)>) {
-//    let (a, mut b) = resources;
-//    b.value += 1;
-//}
-
 
 pub fn minimum_example() {
 
@@ -151,9 +94,9 @@ pub fn minimum_example() {
         ExecuteSequential::new(vec![
 
             // Clean way
-            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_fn),
-            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_fn2),
-            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_fn3),
+            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_inline),
+            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_typedef),
+            acquire_resources_and_use(dispatcher.clone(), world.clone(), example_single_resource),
 
             // Closure is still possible
             acquire_resources_and_use(
@@ -169,30 +112,6 @@ pub fn minimum_example() {
                     })
                 }
             ),
-
-
-
-
-            acquire_resources_and_visit(
-                dispatcher.clone(),
-                world.clone(),
-                |resources: (
-                    ReadBorrow<HelloWorldResourceA>,
-                    WriteBorrow<HelloWorldResourceB>
-                )| {
-                    let (a, mut b) = resources;
-                    println!("a {}", a.value);
-                }
-            ),
-
-/*
-            acquire_resources_and_visit(
-                dispatcher.clone(),
-                world.clone(),
-                example_fn_borrow
-            ),
-            */
         ])
-
     })
 }
